9a3c8028ede0f1225abbbac1679d313e857c2490
Shashidhar Amarannavar <shashidharamarannavar@gmail.com>
2024-08-20 19:32:26 +0530

    miscleneous

--- my-app/src/Homescreen/Organization.jsx
+++ my-app/src/Homescreen/Organization.jsx
@@ -24,24 +24,30 @@ function Organization() {
             </p>
           </div>
           <div className="org-container">
-            <div style={{ width: "400px" }}>
-              <div className="org-list">
-                <div className="org-profile">profile</div>
-                <div>
-                  <p className="org-text">Softsuave Organization PVT LTD</p>
-                  <p style={{ marginTop: "0px", fontWeight: "lighter" }}>
-                    Super Admin
-                  </p>
-                  <p style={{ fontFamily: "sans-serif" }}>
-                    Non adminstrative organization
-                  </p>
-                </div>
-              </div>
-              <div className="edit-container">
-                <p className="edit-org">Edit</p>
-                <p className="edit-org">Delete</p>
-              </div>
-            </div>
+            {Array(3)
+              .fill()
+              .map((_, index) => {
+                return (
+                  <div key={index} style={{ width: "400px" }}>
+                    <div className="org-list">
+                      <div className="org-profile">profile</div>
+                      <div>
+                        <p className="org-text">Softsuave Organization</p>
+                        <p style={{ marginTop: "0px", fontWeight: "lighter" }}>
+                          Super Admin
+                        </p>
+                        <p style={{ fontFamily: "sans-serif" }}>
+                          Non adminstrative organization
+                        </p>
+                      </div>
+                    </div>
+                    <div className="edit-container">
+                      <p className="edit-org">Edit</p>
+                      <p className="edit-org">Delete</p>
+                    </div>
+                  </div>
+                );
+              })}
             {organizationName && (
               <div style={{ width: "400px" }}>
                 <div className="org-list">
@@ -52,7 +58,7 @@ function Organization() {
                       User
                     </p>
                     <p style={{ fontFamily: "sans-serif" }}>
-                     {organizationDescription}
+                      {organizationDescription}
                     </p>
                   </div>
                 </div>
